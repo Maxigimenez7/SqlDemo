14c8c8dc8045dc8ec934543244b0f6275fc468db
MAXI <maxigimenez578@gmail.com>
2024-05-01 16:08:59 -0300

    DIRECCIONES

--- sucursales.sql
+++ sucursales.sql
@@ -1,1 +1,3 @@
 CREATE TABLE sistema.sucursales (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT , direccion VARCHAR(20))
+
+INSERT INTO sistema.sucursales (direccion) VALUES ("san martin 2030"),("directorio 5874"),("av. libertador 4825"),("terrrada 4585"),("galileo 8952")
